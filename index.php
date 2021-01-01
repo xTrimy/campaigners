dbf554d38d1f3fd8968beac3db954f82fb6df8a8
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-01-01 13:29:57 +0200

    Finished Home Page

--- index.php
+++ index.php
@@ -1,94 +1,363 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="layout/css/rockwell.css">
-    <link rel="stylesheet" href="layout/css/master.css">
-    <meta charset="utf-8">
-    <title>Campaigners</title>
-  </head>
-  <body>
-    <div id="header">
-      <div class="nav-buttons">
-        <div class="nb selected">Home</div>
-        <div class="nb">About Us</div>
-        <div class="nb">Blog</div>
-      </div>
-        <div class="logo">
-          <img src="layout/png/logo-white.png" alt="Campaigners Logo White">
-        </div>
-        <div class="nav-buttons">
-          <div class="nb">Watch</div>
-          <div class="nb">Contact Us</div>
-          <div class="nb">Contact Us</div>
-        </div>
-    </div>
-    <div id="top-banner">
-      <div class="content">
-        <h2>Leading The Twenties</h2>
-        <h1>Campaigners</h1>
-      </div>
-    </div>
-      <div class="wrapper">
-      <div id="section1" class="flex-container section">
-        <div class="left" style="flex:4">
-          <h1>What is Campaigners?</h1>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et. Lorem ipsum dolor
-            sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            invidunt ut labore et dolore magna aliquyam erat,
-            sed diam voluptua. At vero eos et. Lorem ipsum dolor sit </p>
 
-          <h2>Mission</h2>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et. Lorem ipsum dolor
-            sit amet, consetet </p>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="layout/css/splide.min.css">
+  <link rel="stylesheet" href="layout/css/rockwell.css">
+  <link rel="stylesheet" href="layout/css/master.css">
+  <meta charset="utf-8">
+  <title>Campaigners</title>
+  <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@2.4.21/dist/js/splide.min.js"></script>
+</head>
 
-          <h2>Vission</h2>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-            aliquyam erat, sed diam voluptua. At vero eos et. Lorem ipsum dolor
-            sit amet, consetet</p>
-          <div class="xbutton">Read More</div>
-        </div>
-        <div class="right" style="flex:2">
-          <!-- Images don't need to be put in h3 tag -->
-          <img src="layout/svg/Tree.svg">
-        </div>
-      </div>
-      <div id="section2" class="flex-container section">
-    <div class="left">
-      <img style="width:100%;" src="layout/png/Photos.png" alt="">
+<body>
+  <div id="header">
+    <div class="nav-buttons">
+      <!-- <div class="nb selected">Home</div> -->
+      <!-- <div class="nb">About Us</div> -->
+      <!-- <div class="nb">Blog</div> -->
     </div>
-    <div class="right">
-      <div class="container">
-        <h1>Our Work</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <div class="xbutton">Take a Look!</div>
-      </div>
+    <div class="logo">
+      <img src="layout/png/logo-white.png" alt="Campaigners Logo White">
+    </div>
+    <div class="nav-buttons">
+      <!-- <div class="nb">Watch</div> -->
+      <!-- <div class="nb">Contact Us</div> -->
+      <!-- <div class="nb">Contact Us</div> -->
     </div>
   </div>
-  <div id="section3 section">
-    <div class="latest-news">
-      <h1 class="center">Latest News</h1>
-      <div class="news-container">
-        <?php for($i = 0; $i < 3; $i++){
-          ?>
-          <div class="item">
-            <div class="image"></div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <div class="xbutton center">
-              Read More
-            </div>
+  <div id="top-banner">
+    <div class="content">
+      <h2>Empowering the new decade</h2>
+      <h1>Campaigners</h1>
+    </div>
+  </div>
+  <div class="wrapper">
+    <div id="section1" class="flex-container section">
+      <div class="left" style="flex:4">
+        <h1>What is Campaigners?</h1>
+        <h2>Mission</h2>
+        <p>We’re on a mission to spread positive behavior easier and more appealing for everyone, everywhere. Helping people to start their campaigns and guiding them to the right path. We do this through groundbreaking strategies that increase knowledge, change policy and shift norms.</p>
+
+        <h2>Vission</h2>
+        <p>Through our nearly 11 years of experience promoting positive behavior, we have developed a comprehensive, evidence-based framework to behavior change marketing that informs all of our work. We’re seeking for perfection for the next decade to deliver our core and the message we’re responsible for.</p>
+      </div>
+      <div class="right" style="flex:2">
+        <!-- Images don't need to be put in h3 tag -->
+        <img src="layout/svg/Tree.svg">
+      </div>
+    </div>
+    <div id="section5">
+      <h1 class="center">Committees</h1>
+      <div class="committees">
+
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/personnel.png">
           </div>
+          <div class="c-desc">
+            <h1>Personnel</h1>
+            <p>
+              Our tech HR<br>
+              It’s “IT” committee with “HR” skills responsible for everything related to our system, managing Campaigners’ website, collecting data, creating forms, evaluation of members, monitoring rules, and solving problems.
+            </p>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-desc">
+            <h1>Fundraising</h1>
+            <p>
+              Our mains suppliers.
+              <br>
+              They’re responsible for collecting money, holding the club’s budget, reaching out sponsors for the treats on campus and events.
+            </p>
+          </div>
+          <div class="c-image">
+            <img src="layout/png/fundraising.png">
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/pr.png">
+          </div>
+          <div class="c-desc">
+            <h1>Public Relations</h1>
+            <p>
+              The image of the club.<br>
+              They’re responsible for managing all our social media platforms, Branding our name online and offline, doing online and offline campaigns and for any collaborations.
+            </p>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-desc">
+            <h1>Multimedia</h1>
+            <p>
+              Our cameramen.
+              <br>
+              They’re responsible for photography, videography, design and all the media coverage on events throughout the year.
+            </p>
+          </div>
+          <div class="c-image">
+            <img src="layout/png/media.png">
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/coordination.png">
+          </div>
+          <div class="c-desc">
+            <h1>Coordination</h1>
+            <p>
+              Our creativity makers
+              <br>
+              They’re responsible for planning and organizing our events, activities and booths. Also their work reflects our image.
+            </p>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-desc">
+            <h1>Coaching</h1>
+            <p>
+              Our drillers
+              <br>
+              They’re responsible for teaching how te be a leader, marketing a behavior to the audience and also making a campaign from A to Z.
+            </p>
+          </div>
+          <div class="c-image">
+            <img src="layout/png/coaching.png">
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/headmaster.png">
+          </div>
+          <div class="c-desc">
+            <h1>Headmaster </h1>
+            <p>
+              Our trainers
+              <br>
+              It’s an academic committee consisting of four schools “ Entrepreneurship - Humanities - Philosophy - Psychology “<br>
+              All of them have the same aim which is trying to change the negative behaviors and develop all your inner skills.
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div id="section6" style="margin-top:100px;">
+      <h1 class="center">Schools</h1>
+      <div class="committees">
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/brain.png">
+          </div>
+          <div class="c-desc">
+            <h1>Psychology</h1>
+            <p>
+              This school will not be only about body languages, but it will consist of psychological theories and how everything affects the human being and his mental state
+            </p>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-desc">
+            <h1>Entrepreneurship</h1>
+            <p>
+              An entrepreneur is a person who is willing to develop, risk, manage and create a business project by his/her own. This school will be keen on showing how to reach success not how to earn money.
+            </p>
+          </div>
+          <div class="c-image">
+            <img src="layout/png/idea.png">
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-image">
+            <img src="layout/png/atom.png">
+          </div>
+          <div class="c-desc">
+            <h1>Humanities</h1>
+            <p>
+              This school is about the science of sciences, it includes everything in life, it includes everything related to human beings, and to life as a whole, this year’s aim is how to be unique in a normal world.
+            </p>
+          </div>
+        </div>
+
+        <div class="item">
+          <div class="c-desc">
+            <h1>Philosophy</h1>
+            <p>
+              Philosophy is the love of wisdom, philosophy will not only include philosophical theories, it will include mythologies, and it will help in finding answers to a-lots of questions in our minds.
+            </p>
+          </div>
+          <div class="c-image">
+            <img src="layout/png/philosophy.png">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- <div id="section2" class="flex-container section">
+      <div class="left">
+        <img style="width:100%;" src="layout/png/Photos.png" alt="">
+      </div>
+      <div class="right">
+        <div class="container">
+          <h1>Our Work</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <div class="xbutton">Take a Look!</div>
+        </div>
+      </div>
+    </div> -->
+
+    <!-- <div id="section3 section">
+      <div class="latest-news">
+        <h1 class="center">Latest News</h1>
+        <div class="news-container">
+          <?php for ($i = 0; $i < 3; $i++) {
+          ?>
+            <div class="item">
+              <div class="image"></div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+              <div class="xbutton center">
+                Read More
+              </div>
+            </div>
           <?php
-        } ?>
+          } ?>
         </div>
         <div class="xbutton center">View All</div>
+      </div>
+    </div> -->
+
+    <div id="section4" class="section">
+      <div class="team">
+        <h1 class="center">Our Team</h1>
+        <div class="splide">
+
+          <div class="splide__track">
+            <ul class="splide__list">
+              <?php
+              $board = [
+                [
+                  "name" => "Habiba Hisham",
+                  "position" => "President"
+                ],
+                [
+                  "name" => "Mahmoud Zoair",
+                  "position" => "Vice President"
+                ],
+                [
+                  "name" => "Nada Ammar",
+                  "position" => "OC Manager"
+                ],
+                [
+                  "name" => "Mohanad",
+                  "position" => "Headmaster Head"
+                ],
+                [
+                  "name" => "Sara Nabil",
+                  "position" => "Personnel Head"
+                ],
+                [
+                  "name" => "Mohamed Ashraf",
+                  "position" => "Personnel Co-Head"
+                ],
+                [
+                  "name" => "Yasmine Essam",
+                  "position" => "Coaching Head"
+                ],
+                [
+                  "name" => "Osary",
+                  "position" => "Coaching Co-Head"
+                ],
+                [
+                  "name" => "Mohamed Mostafa",
+                  "position" => "Media Head"
+                ],
+                [
+                  "name" => "Yossef Essam",
+                  "position" => "Media Co-Head"
+                ],
+                [
+                  "name" => "Hanine Tawfik",
+                  "position" => "PR Head"
+                ],
+                [
+                  "name" => "Mohamed Selim",
+                  "position" => "Coordination Head"
+                ],
+                [
+                  "name" => "Carole Barsoum",
+                  "position" => "Coordination Co-Head"
+                ],
+                [
+                  "name" => "Habiba Walid",
+                  "position" => "Fundraising Head"
+                ],
+              ];
+              foreach ($board as $b => $d) {
+              ?>
+                <li class="splide__slide">
+                  <div class="item">
+                    <div class="image">
+                      <img src="layout/jpg/board/<?php echo strtolower(str_replace(" ", "-", $d['name'])); ?>.jpeg">
+                    </div>
+                    <div class="name"><?php echo $d['name']; ?></div>
+                    <div class="position"><?php echo $d['position']; ?></div>
+                  </div>
+                </li>
+              <?php
+              }
+              ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="section7">
+      <h1 class="center">Previous Events</h1>
+      <div class="splide" id="gallery">
+        <div class="splide__track">
+          <ul class="splide__list">
+            <?php
+            for ($i = 1; $i <= 12; $i++) {
+            ?>
+              <li class="splide__slide">
+                <img src="layout/jpg/events/<?php echo $i; ?>.jpeg">
+              </li>
+            <?php
+            }
+            ?>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
-    </div>
-  </body>
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      new Splide('.splide', {
+        fixedWidth: 400,
+        height: 400,
+        gap: 10,
+        rewind: true,
+        cover: true,
+        pagination: false,
+      }).mount();
+      new Splide('#gallery').mount();
+    });
+  </script>
+  <div id="footer">
+    Made with <img src="layout/svg/like.svg" alt="love" width="20px"> By Personnel Team &copy; 2020
+  </div>
+</body>
 </html>
